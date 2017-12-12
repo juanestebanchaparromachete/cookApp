7f969d30f5930e87cc54b71b208c5b03d2d77768
lfplazas10 <lf.plazas10@uniandes.edu.co>
2017-12-11 22:22:46 -0500

    Comments
    Muy buen trabajo, código organizado, fácil de leer y bien hecho.

--- The Food Venue/imports/ui/Landing.jsx
+++ The Food Venue/imports/ui/Landing.jsx
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { Meteor } from 'meteor/meteor';
-import classnames from 'classnames';
+import classnames from 'classnames'; 
 
 // Task component - represents a single todo item
 export default class Landing extends Component {
