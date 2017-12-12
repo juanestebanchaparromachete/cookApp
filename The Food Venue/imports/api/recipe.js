3660c52ee7117870d497b24f58a93017b8b56fde
Santiago Rojas Herrera <s.rojas19@uniandes.edu.co>
2017-12-11 22:35:36 -0500

    Correcciones en recipe.js
    Pasar objeto por parametro en recipes.insert, revisar adecuadamente si el usuario esta autenticado, y calcular bien el puntaje de una receta.

--- The Food Venue/imports/api/recipe.js
+++ The Food Venue/imports/api/recipe.js
@@ -5,25 +5,31 @@ import { check } from 'meteor/check';
 export const Recipes = new Mongo.Collection('recipes');
 
 if (Meteor.isServer) {
-  // This code only runs on the server
-  // Only publish tasks that are public or belong to the current user
   Meteor.publish('recipes', function recipesPublication() {
     return Recipes.find({});
   });
 }
 
 Meteor.methods({
+    /* srojas19: Aunque no afecta la funcionalidad, se debe procurar recibir un objeto con todos los
+       atributos por parametro, para que sea mas facil mantener consistencia entre distintos
+       componentes de la aplicacion
+       ej: 'recipes.insert' (recipe) { ... }
+    */
     'recipes.insert'(userId, name, description, process,video, ingredients,typeOfFood , country) {
         check(name, String);
     
-        if (!this.userId) {
+        // srojas19: Corrección en revisión de id de usuario
+        if (!this.userId()) {
           throw new Meteor.Error('not-authorized');
         }
     
+        // srojas19: De recibir la receta como objeto por parametro:
+        // Recipes.insert({recipe, ...}
         Recipes.insert({
           name,
           userID: userId,
-          username: Meteor.users.findOne(this.userId).username,
+          username: Meteor.user().username, // srojas19: Mejora para acceder al username del usuario en sesion
           createdAt: new Date(),
           description,
           process,
@@ -38,7 +44,8 @@ Meteor.methods({
   ,'recipes.comment'(nameUser, text, recipeId) {
     check(text, String);
 
-    if (!this.userId) {
+    // srojas19: Corrección en revisión de id de usuario
+    if (!this.userId()) {
       throw new Meteor.Error('not-authorized');
     }
 
@@ -51,11 +58,13 @@ Meteor.methods({
 ,
 'recipes.rate'( vote, recipeId) {
     check(recipeId, String);
-
-    if (!this.userId) {
+    // srojas19: Corrección en revisión de id de usuario
+    if (!this.userId()) {
       throw new Meteor.Error('not-authorized');
     }
     const chef2 = Recipes.findOne(recipeId);
+    
+    // srojas19: Sospecho que este rating esta mal calculado
     var rate = ((chef2.rating + vote)/2) ;
    
     Recipes.update(recipeId, { $set: { rating: rate } });
